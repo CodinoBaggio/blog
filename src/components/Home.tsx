c41f6fd3ca5dad2ff6ede65adc9185d0a71dc8ce
CodinoBaggio <32092103+CodinoBaggio@users.noreply.github.com>
2023-07-01 11:31:17 +0900

    完成？

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Home.css';
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
+import { db } from '../firebase';
 
 export const Home = () => {
-  return <div>Home</div>;
+  const [postList, setPostList] = useState<any>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+
+  console.log(postList);
+
+  const handleDelete = async (id: string) => {
+    await deleteDoc(doc(db, 'posts', id));
+  };
+
+  return (
+    <div className="homePage">
+      {postList.map((post: any) => (
+        <div className="postContents" key={post.id}>
+          <div className="postTitle">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postText">{post.postText}</div>
+          <div className="nameAn">
+            <h3>{post.auther.username}</h3>
+            <button onClick={(e) => handleDelete(post.id)}>削除</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
